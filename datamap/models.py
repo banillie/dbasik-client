d3831b1d1352cbf8df88668c98c799fdffcd24b0
Matthew Lemon <y@yulqen.org>
2024-06-07 17:17:36 +0100

    Adds datatype to the datamapline model

--- datamap/models.py
+++ datamap/models.py
@@ -10,5 +10,6 @@ class DatamapLine(models.Model):
     dm = models.ForeignKey(Datamap, on_delete=models.CASCADE, blank=False)
     key = models.CharField(max_length=56, blank=False, null=False)
     sheet = models.CharField(max_length=56, blank=False, null=False)
+    datatype = models.CharField(max_length=24, blank=True, null=True)
     cellref = models.CharField(max_length=5, blank=False, null=False)
 
